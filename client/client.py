5d31391e667a64b5f4ac01985edcd0267ca42fe7
darahask <darahask12@gmail.com>
2021-09-15 22:23:06 +0530

    beta release

--- client/client.py
+++ client/client.py
@@ -1,37 +1,46 @@
+# importing Libraries
 import socket
 import pickle
 import time
 
-data = input("Enter the server adress: ")
-
+# socket size and server addresses
+data = input("Enter the socket ftp-server address: ")
 IP_ADD = data
 PORT = 2222
 ADD_T = (IP_ADD, PORT)
 SIZE = 2048
 
-
+# returns a dictionary for pickle to serialize
 def constructdata(type, ip, port, data):
     dict = {"command": type, "ip": ip, "port": port, "data": data}
     return dict
 
 
+# INFO: Pickle is used to serialize data, loads will de-serialize and dumps will serialize
+
+# main function
 def main():
+    # establishing connection to remote server
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADD_T)
 
     while True:
+        # waiting for input
         data = pickle.loads(client.recv(SIZE))
         cmd = data["command"]
-        print(data)
 
+        # if cmd == msg, print the data sent by server
         if cmd == "msg":
             print(data["data"])
 
         cmd = input("> ")
 
+        # if cmd == list, a list request is sent to server whch returns active clients
         if cmd == "list":
             dict = {"command": "list", "ip": "", "port": "", "data": ""}
             client.send(pickle.dumps(dict))
+
+        # if cmd == upload, file is uploaded to the desired client
         if cmd == "upload":
             reciever = input("Enter Client IP > ")
             port = input("Enter Client Port > ")
@@ -39,16 +48,19 @@ def main():
 
             filename = file_path.split("/")[-1]
 
+            # pickle is used to serialize the data, and a handshake with file name is done
             client.send(pickle.dumps(constructdata("upload", reciever, port, filename)))
-            print("1")
+
             client.recv(SIZE)
-            print("3")
+            # after server responds ok, the file starts uploading
             with open(file_path, "r") as f:
                 while True:
                     data_stream = f.read(SIZE)
                     if not data_stream:
                         break
                     client.send(data_stream.encode("utf-8"))
+
+                # after file is sent a command done is sent to mark the end of the upload
                 time.sleep(0.001)
                 dict = {
                     "command": "done",
@@ -58,7 +70,9 @@ def main():
                 }
                 client.send(pickle.dumps(dict))
 
-        if cmd == "recieve":
+        # if cmd == receive, the client is set to receive mode to download data sent by another client
+        if cmd == "receive":
+            # file with <filename> is created
             filename = pickle.loads(client.recv(SIZE))["data"]
             with open(filename, "a") as f:
                 while True:
@@ -68,6 +82,8 @@ def main():
                         f.write(x)
                     except:
                         break
+
+        # close the client
         if cmd == "stop":
             break
 
--- server/server.py
+++ server/server.py
@@ -1,90 +1,120 @@
+# importing libraries
 import socket
 import threading
 import pickle
 import time
 
+# configuring ip address of server
 IP_ADD = "0.0.0.0"
 PORT = 2222
 ADD_T = (IP_ADD, PORT)
 SIZE = 2048
+
+# a dictionary for storing (address,client) key-value pairs
 clients = {}
 
-
+# returns a dictionary for pickle to serialize
 def constructdata(type, ip, port, data):
     dict = {"command": type, "ip": ip, "port": port, "data": data}
     return dict
 
 
+# forwards data to a client at an address
 def sendto(data, address):
     print(address)
     sendcon = clients[address]
     sendcon.send(data)
 
 
+# a thread function which handles the logic of file transfer
 def handle_transfer(con, add):
+    # sending welcome message to client
     print(f"Client at {add} is connected")
     con.send(pickle.dumps(constructdata("msg", "", "", "Welcome to file transfer !!!")))
 
-    while True:
-        info = pickle.loads(con.recv(SIZE))
-        action = info["command"]
+    try:
+        # accept and decode requests by client
+        while True:
+            info = pickle.loads(con.recv(SIZE))
+            action = info["command"]
 
-        if action == "list":
-            ppl = "Active Users: \n"
+            # sends list of active users to client
+            if action == "list":
+                ppl = "Active Users: \n"
 
-            for k, v in clients:
-                ppl += f"{k}: {v}\n"
+                for k, v in clients:
+                    ppl += f"{k}: {v}\n"
 
-            con.send(pickle.dumps(constructdata("msg", "", "", ppl)))
+                con.send(pickle.dumps(constructdata("msg", "", "", ppl)))
 
-        if action == "upload":
-            ip = info["ip"]
-            port = info["port"]
-            filename = info["data"]
-            print(filename)
-            sendto(pickle.dumps(constructdata("", "", "", filename)), (ip, int(port)))
-            con.send("OK".encode("utf-8"))
-            print(2)
-            while True:
-                co = con.recv(SIZE)
-                try:
-                    x = pickle.loads(co)
-                    print(5)
-                    if x["command"] == "done":
-                        print(6)
+            # transfering data from one client to another
+            if action == "upload":
+                # storing the ip address and port number of the client to send and sending headers
+                ip = info["ip"]
+                port = info["port"]
+                filename = info["data"]
+
+                # sending file-name to the recevier client
+                sendto(
+                    pickle.dumps(constructdata("", "", "", filename)), (ip, int(port))
+                )
+                # sending a confirmation to sender client
+                con.send("OK".encode("utf-8"))
+
+                # forwarding data content packets to receiver client
+                while True:
+                    co = con.recv(SIZE)
+                    try:
+                        # if the data is of header type(JSON) then close the connection
+                        x = pickle.loads(co)
+
+                        if x["command"] == "done":
+                            # sending close instruction to receiver client
+                            sendto(
+                                pickle.dumps(constructdata("", "", "", "")),
+                                (ip, int(port)),
+                            )
+                            time.sleep(0.001)
+
+                            # notifyting sender and receiver that the file transfer is complete
+                            # here con is instance of sender client, receiver clients instance is
+                            # fetched using the dictionary
+                            sendto(
+                                pickle.dumps(
+                                    constructdata("msg", "", "", "file transfer done")
+                                ),
+                                (ip, int(port)),
+                            )
+
+                            con.send(
+                                pickle.dumps(constructdata("msg", "", "", "file sent"))
+                            )
+                            break
+                    except:
+                        # id data is content type(utf-8) forward it to receiver client
                         sendto(
-                            pickle.dumps(constructdata("", "", "", "")),
-                            (ip, int(port)),
+                            co,
+                            (info["ip"], int(info["port"])),
                         )
-                        time.sleep(0.001)
-                        sendto(
-                            pickle.dumps(
-                                constructdata("msg", "", "", "file transfer done")
-                            ),
-                            (ip, int(port)),
-                        )
-                        con.send(
-                            pickle.dumps(constructdata("msg", "", "", "file sent"))
-                        )
-                        break
-                except:
-                    print(4)
-                    sendto(
-                        co,
-                        (info["ip"], int(info["port"])),
-                    )
+    except:
+        # client is disconnected
+        print(f"Client at {add} is disconnected")
 
 
+# starting the lifecycle of the socket server, create->bind->listen->accept
 def start_server():
     tserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tserver.bind(ADD_T)
     tserver.listen()
     print(f"Server started")
-
+    # accepts multiple incoming connections
     while True:
         con, add = tserver.accept()
+        # add all client objects to dictionary
         clients[add] = con
+        # enable seperate thread for each connection
         thread = threading.Thread(target=handle_transfer, args=(con, add))
+        # starting thread
         thread.start()
 
 
